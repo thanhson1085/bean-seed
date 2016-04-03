d191ecebe0f1d25a6e6ff4506d733f64a6f8f2cc
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-03 05:58:44 +0000

    first lession

---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+var server = app.listen('3000', function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server start at http://%s:%s', '0.0.0.0', 3000);
+});
+
+// say hello!!
+app.get('/hello', function(req, res){
+    res.send(JSON.stringify({'hello': 'world !!!'}));
+});
