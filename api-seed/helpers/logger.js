fbfd4029032823774bbe56852087ef482189779c
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-04 16:55:30 +0000

    add Logger Winston

---
+++ api-seed/helpers/logger.js
@@ -0,0 +1,52 @@
+'use strict';
+var winston = require('winston');
+winston.emitErrs = true;
+
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: 'debug', 
+            handleExceptions: false,
+            json: false,
+            colorize: false,
+            timestamp: function() {
+                var date = new Date();                          
+
+                var hour = date.getUTCHours();              
+                hour = (hour < 10 ? '0' : '') + hour;   
+
+                var min  = date.getUTCMinutes();
+                min = (min < 10 ? '0' : '') + min;
+
+                var sec  = date.getUTCSeconds();
+                sec = (sec < 10 ? '0' : '') + sec;
+
+                var year = date.getUTCFullYear();
+
+                var month = date.getUTCMonth() + 1;
+                month = (month < 10 ? '0' : '') + month;
+
+                var day  = date.getUTCDate();
+                day = (day < 10 ? '0' : '') + day;
+
+                var millisecond = date.getUTCMilliseconds();
+
+                return year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec + '.' + millisecond;
+
+            },
+            formatter: function(options) {
+                // Return string will be passed to logger.
+                return options.timestamp() + ' ' + options.level.toUpperCase() +' '+ (undefined !== options.message ? options.message : '') +
+                    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+            }
+        }),
+    ],
+    exitOnError: false
+});
+
+module.exports = logger;
+module.exports.stream = {
+    write: function(message){
+        logger.info(message);
+    }
+};
--- api-seed/index.js
+++ api-seed/index.js
@@ -3,11 +3,12 @@
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var logger = require('./helpers/logger');
 
 var server = app.listen('3000', function () {
     var host = server.address().address;
     var port = server.address().port;
-    console.log('Server start at http://%s:%s', '0.0.0.0', 3000);
+    logger.info('Server start at http://%s:%s', '0.0.0.0', 3000);
 });
 
 // say hello!!
