973464a235de7954cb89b90245fe206629c65e5e
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-05 10:43:57 +0000

    add config management

--- api-seed/helpers/logger.js
+++ api-seed/helpers/logger.js
@@ -1,12 +1,13 @@
 'use strict';
 var winston = require('winston');
+var config = require('config');
 winston.emitErrs = true;
 
 var logger = new winston.Logger({
     transports: [
         new winston.transports.Console({
-            level: 'debug', 
-            handleExceptions: false,
+            level: config.get('logs.level'),
+            handleExceptions: config.get('logs.handleExceptions'),
             json: false,
             colorize: false,
             timestamp: function() {
