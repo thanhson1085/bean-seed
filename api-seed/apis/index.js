291cdcf50d4ba835e74d75fa8de2ad62ed1c8b05
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-11 07:40:20 +0000

    correct swagger added

--- api-seed/apis/index.js
+++ api-seed/apis/index.js
@@ -4,9 +4,4 @@ var express = require('express'),
 
 router.use('/api/v1/users', require('./users'));
 
-// nothing for root
-router.get('/', function(req, res){
-    res.send(JSON.stringify({}));
-});
-
 module.exports = router;
