8341a0f5b9b555b1a47b7b6416c8059eb984779a
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-13 02:11:16 +0000

    login api

--- api-seed/apis/users.js
+++ api-seed/apis/users.js
@@ -1,6 +1,10 @@
 'use strict';
 var express = require('express'), 
     db = require('../models'),
+    logger = require('../helpers/logger'),
+    moment = require('moment'),
+    config = require('config'),
+    crypto = require('crypto'),
     router = express.Router();
 
 // create a new user
@@ -10,9 +14,46 @@ router.post('/create', function(req, res){
         if (error) {
             return res.status(406).send(JSON.stringify({error}));
         }
-        delete new_user['salt'];
-        delete new_user['password'];
         res.send(JSON.stringify(new_user));
+    });
+});
+
+// login
+router.post('/login', function(req, res){
+    var username = req.body.username;
+    var password = req.body.password;
+    db.User.findOne({
+        username: username
+    }).then(function(user){
+        if (!user.authenticate(password)) {
+            throw false;
+        }
+        db.Token.findOne({
+            username: username
+        }).then(function(t){
+            if (!t){
+                crypto.randomBytes(64, function(ex, buf) {
+                    var token = buf.toString('base64');
+                    var today = moment.utc();
+                    var tomorrow = moment(today).add('seconds', config.get('token_expire'));
+                    var token = new db.Token({
+                        username: username,
+                        token: token,
+                        expiredAt: tomorrow
+                    });
+                    token.save(function(error, to){
+                        res.send(JSON.stringify(to));
+                    });
+                });
+            }
+            res.send(JSON.stringify({
+                token: t.token,
+                id: user.id,
+                expiredAt: t.expiredAt
+            }));
+        });
+    }).catch(function(e){
+        res.status(401).send(JSON.stringify(e));
     });
 });
 
