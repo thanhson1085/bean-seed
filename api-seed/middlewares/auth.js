2e372b171e124a6fc8975171c603e4b11ef6b1a7
Nguyen Sy Thanh Son <sonnst@sigma-solutions.eu>
2016-04-14 07:20:08 +0000

    authenticate apis

---
+++ api-seed/middlewares/auth.js
@@ -0,0 +1,33 @@
+'use strict';
+var db = require('../models');
+var _ = require('lodash');
+var config = require('config');
+var logger = require('../helpers/logger');
+
+module.exports = function(req, res, next) {
+    if (req.method === 'OPTIONS') {
+        return next();
+    }
+    var t = req.get('Authorization');
+    var unauthorization = config.get('unauthorization');
+    if (_.indexOf(unauthorization, req.url) < 0){
+        if (t === undefined) {
+            logger.debug('Access Denied');
+            return res.status(401).send(JSON.stringify({}));
+        }
+        t = t.replace('Bearer ', '');
+        db.Token.findOne({
+            token: t
+        }).then(function(token){
+            if (!token){
+                throw('');
+            }
+            return next();
+        }).catch(function(){
+            logger.debug('Access Denied %s !!!', t);
+            return res.status(401).send(JSON.stringify({}));
+        });
+    } else {
+        return next();
+    }
+};
