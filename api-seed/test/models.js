907a090916b7300515424dd6fa89e2c063f20187
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-04-24 21:36:12 +0700

    Create models.js

---
+++ api-seed/test/models.js
@@ -0,0 +1,24 @@
+var chai = require('chai');
+var should = chai.should();
+var User = require('../apis/models/user');
+
+describe('User Model', function() {
+    it('should create a new user', function(done) {
+        var user = new User({
+            email: 'test@gmail.com',
+            password: 'password'
+        });
+        user.save(function(err) {
+            if (err) return done(err);
+            done();
+        })
+    });
+    it('should delete a user', function(done) {
+        User.remove({
+            email: 'test@gmail.com'
+        }, function(err) {
+            if (err) return done(err);
+            done();
+        });
+    });
+});
