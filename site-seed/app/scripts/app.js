5d78f8bb0d13370047d3cbb1837511100865aba8
thanhson1085 <sonnst@sigma-solutions.eu>
2016-04-29 11:03:12 +0000

    init angular

---
+++ site-seed/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name siteSeedApp
+ * @description
+ * # siteSeedApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('siteSeedApp', [
+    'ngTouch'
+  ]);
---
+++ site-seed/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name siteSeedApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the siteSeedApp
+ */
+angular.module('siteSeedApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
