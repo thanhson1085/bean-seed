944cdf63a2a6383077302ae4cf87fa07832202f0
thanhson1085 <sonnst@sigma-solutions.eu>
2016-05-05 11:11:17 +0000

    adding sb admin 2

--- site-seed/app/scripts/app.js
+++ site-seed/app/scripts/app.js
@@ -9,6 +9,43 @@
  * Main module of the application.
  */
 angular
-  .module('siteSeedApp', [
-    'ngTouch'
-  ]);
+.module('siteSeedApp', [
+    'ngTouch',
+    'ui.router',
+    'oc.lazyLoad'
+])
+.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    '$ocLazyLoadProvider', 
+    '$httpProvider',
+    function ($stateProvider,$urlRouterProvider,$ocLazyLoadProvider, $httpProvider) {
+
+        $ocLazyLoadProvider.config({
+            debug:false,
+            events:true,
+        });
+
+        $urlRouterProvider.otherwise('/login');
+
+        $stateProvider
+        .state('login',{
+            templateUrl:'views/pages/login.html',
+            controller: 'LoginCtrl',
+            controllerAs: 'vm',
+            url:'/login',
+            resolve: {
+                loadMyDirectives:function($ocLazyLoad){
+                    return $ocLazyLoad.load(
+                        {
+                            name:'siteSeedApp',
+                            files:[
+                                'scripts/controllers/login.js'
+                            ]
+                        });
+                }
+            }
+        })
+    }
+]);
+
