5e21b4c961345e3375e2c1c845f577212d4f18c4
thanhson1085 <sonnst@sigma-solutions.eu>
2016-05-05 11:11:17 +0000

    adding sb admin 2

---
+++ site-seed/app/scripts/controllers/login.js
@@ -0,0 +1,9 @@
+'use strict';
+// LoginCtrl
+angular.module('siteSeedApp')
+.controller('LoginCtrl', function() {
+    var vm = this;
+    // logout before login
+    vm.login = function login(){
+    };
+});
--- site-seed/test/karma.conf.js
+++ site-seed/test/karma.conf.js
@@ -24,6 +24,8 @@ module.exports = function(config) {
       'bower_components/angular/angular.js',
       'bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js',
       'bower_components/angular-touch/angular-touch.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/oclazyload/dist/ocLazyLoad.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
