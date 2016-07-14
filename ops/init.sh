6f1a31a53a638a0eb3c796557bcc1844aa4eb85d
thanhson1085 <sonnst@sigma-solutions.eu>
2016-07-14 04:06:02 +0000

    add wait command

--- ops/init.sh
+++ ops/init.sh
@@ -49,6 +49,8 @@ echo Installing Docker Compose
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+wait
+
 echo Installing Docker Swarm/Registrator/cAdvisor...
 docker pull swarm
 docker pull registrator
