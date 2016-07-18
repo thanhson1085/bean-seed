2cfff6d0edced8fe2f77f52190d85725727e8260
thanhson1085 <sonnst@sigma-solutions.eu>
2016-07-18 07:07:07 +0000

    init consul consul template

--- ops/nodeone.sh
+++ ops/nodeone.sh
@@ -2,3 +2,11 @@
 
 sudo su -
 
+# Setup Consul
+mkdir -p /etc/consul.d
+cp /build/consul.conf /etc/init/
+start consul
+
+# Setup Consul-Template
+cp /build/consul-tempalte.conf /etc/init/
+start consul-template
