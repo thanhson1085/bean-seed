57a534dd2b52338fe24a171ec1ac2aa1459dc9ff
thanhson1085 <sonnst@sigma-solutions.eu>
2016-07-19 04:45:35 +0000

    add registrator

--- ops/nodeone.sh
+++ ops/nodeone.sh
@@ -7,9 +7,21 @@ mkdir -p /etc/consul.d
 cp /build/consul.conf /etc/init/
 start consul
 
+wait
+
 # Setup Consul-Template
 cp /build/consul-template.conf /etc/init/
 start consul-template
+
+# Restart Docker with new Configuration
+cp /build/docker.conf /etc/default/docker
+service restart docker
+
+wait 
+
+# Setup Registrator
+cp /build/registrator.conf /etc/init/
+start registrator
 
 # Setup Swarm Node
 cp /build/swarm.conf /etc/init/
