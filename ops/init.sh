1b38f7dcf83676d58e8a7409e3ba4b4487fb8c1b
thanhson1085 <thanhson1085@gmail.com>
2016-07-20 17:22:29 +0700

    issue Ubuntu :bug:

--- ops/init.sh
+++ ops/init.sh
@@ -6,6 +6,9 @@ COMPOSE_VERSION=1.7.1
 CONSUL_VERSION=0.6.4
 CONSUL_TEMPLATE_VERSION=0.15.0
 
+# Issue http://askubuntu.com/questions/41605/trouble-downloading-packages-list-due-to-a-hash-sum-mismatch-error
+rm -rf /var/lib/apt/lists/*
+
 echo Installing dependencies...
 apt-get update && \
     apt-get install -y unzip curl wget
