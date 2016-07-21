8f81bbaec350af514ecd7d484c40e13dfce03913
thanhson1085 <sonnst@sigma-solutions.eu>
2016-07-21 06:31:04 +0000

    bug wrong path :bug:

--- ops/nodetwo.sh
+++ ops/nodetwo.sh
@@ -4,25 +4,25 @@ sudo su -
 
 # Setup Consul
 mkdir -p /etc/consul.d
-cp /build/ops/two/consul.conf /etc/init/
+cp /build/ops/data/two/consul.conf /etc/init/
 start consul
 
 wait
 
 # Setup Consul-Template
-cp /build/ops/two/consul-template.conf /etc/init/
+cp /build/ops/data/two/consul-template.conf /etc/init/
 start consul-template
 
 # Restart Docker with new Configuration
-cp /build/ops/two/docker.conf /etc/default/docker
+cp /build/ops/data/two/docker.conf /etc/default/docker
 service docker restart
 
 wait
 
 # Setup Registrator
-cp /build/ops/two/registrator.conf /etc/init/
+cp /build/ops/data/two/registrator.conf /etc/init/
 start registrator
 
 # Setup Swarm Node
-cp /build/ops/two/swarm.conf /etc/init/
+cp /build/ops/data/two/swarm.conf /etc/init/
 start swarm
