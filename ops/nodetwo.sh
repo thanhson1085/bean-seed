f62d915b3e9171442bfe2290cb45c4cfb8d2be50
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-07-24 18:42:21 +0700

    correct registrator :mushroom:

--- ops/nodetwo.sh
+++ ops/nodetwo.sh
@@ -19,6 +19,10 @@ service docker restart
 
 wait
 
+# Create Network Name
+docker network create --driver=overlay build_back-net
+docker network create --driver=overlay build_front-net
+
 # Setup Registrator
 cp /build/ops/data/two/registrator.conf /etc/init/
 start registrator
@@ -29,3 +33,7 @@ start swarm
 
 # Pulling The Images...
 cd /build/ && docker-compose pull
+
+# Run Services
+DOCKER_HOST=tcp://172.20.20.20:12375
+cd /build/ && docker-compose up -d
