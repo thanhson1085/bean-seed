ed9017a787116a5c4d37a24395138c4cdee4af4a
thanhson1085 <sonnst@sigma-solutions.eu>
2016-09-01 09:07:18 +0000

    bash handle sigint sigterm :dragon:

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+_interupt() { 
+    echo "Shutdown $child_proc"
+    kill -TERM "$child_proc" 2>/dev/null
+    exit
+}
+
+trap _interupt INT TERM
 
 workdir=${PWD} 
 
 cd $workdir/api-seed && nodemon index.js &
+child_proc=$! 
 cd $workdir/site-seed && grunt serve
+child_proc="$child_proc $!"
