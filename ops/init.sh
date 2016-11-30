a7220d2693e4894229af7777f21c3fbb10f15c67
Nguyen Sy Thanh Son <thanhson1085@gmail.com>
2016-11-30 03:41:47 +0000

    add dev to docker group :whale:

--- ops/init.sh
+++ ops/init.sh
@@ -10,7 +10,7 @@ git clone https://github.com/thanhson1085/bean-seed.git /build
 wait 
 
 echo Installing Docker ...
-wget -qO- https://get.docker.com/ | sh
+wget -qO- https://get.docker.com/ | sh && usermod -aG docker dev
 
 echo Installing Docker Compose
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
